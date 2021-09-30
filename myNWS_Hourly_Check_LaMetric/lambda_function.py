46326727cc46bde173bec8f767939836b747f81b
Robert Romero <1013414+NessieCanCode@users.noreply.github.com>
2021-09-30 09:01:29 -0700

    Update lambda_function.py

--- myNWS_Hourly_Check_LaMetric/lambda_function.py
+++ myNWS_Hourly_Check_LaMetric/lambda_function.py
@@ -6,30 +6,24 @@ def lambda_handler(event, context):
     county_code = event['queryStringParameters']['county_code']
     if str(county_code) != "demo":
         url = "https://api.weather.gov/alerts/active/zone/" + county_code
+        response = requests.get(url)
+        data = response.json()
+        if None in data['features']:
+            nws_alert_headline = data['title']
+            nws_alert_description = "There are no active watches, warnings or advisories"
+            nws_icon = 46935
+        else:
+            nws_alert_headline = data['features'][0]['properties']['headline']
+            nws_alert_description = data['features'][0]['properties']['description']
+            nws_icon = 16701
+        
     else:
         url = "https://api.weather.gov/alerts?active=true&status=actual&message_type=alert"
-
-    response = requests.get(url)
-    data = response.json()
-    if data['features'][0]['properties']['headline'] == None:
-            try:
-                for i in range(0, 11):
-                    if data['features'][i]['properties']['headline'] != none:
-                        nws_alert_headline = data['features'][i]['properties']['headline']
-                        nws_alert_description = data['features'][i]['properties']['description']
-                        nws_icon = 16701
-                    else:
-                        nws_alert_headline = data['title']
-                        nws_alert_description = "There are no active watches, warnings or advisories"
-                        nws_icon = 46935
-            except:
-                nws_alert_headline = data['title']
-                nws_alert_description = "There are no active watches, warnings or advisories"
-                nws_icon = 46935
-    else:
+        response = requests.get(url)
+        data = response.json()
         nws_alert_headline = data['features'][0]['properties']['headline']
         nws_alert_description = data['features'][0]['properties']['description']
-        nws_icon = 16701    
+        nws_icon = 16701
 
     frame_duration = len(nws_alert_description.split()) / 4 + 2
     api_response = {
